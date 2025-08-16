1922e6652ecd427760c3b933cddc21391dd14843
Raushan kumar <raushan.kumar@tappp.com>
2025-08-16 15:13:12 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React,{ Component } from 'react'
+// import reactLogo from './assets/react.svg'
+import './App.css'
+import HomePage from './Components/HomePage.Component'
+
+const App = () => {
+  
+  return (
+    <div>
+      <HomePage />
+    </div>
+  )
+}
+
+export default App
